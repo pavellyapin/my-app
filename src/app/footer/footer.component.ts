554945b6bcc30cd46a4c9e2099db586d0a42cd20
pavellyapin <mr.pavellyapin@gmail.com>
2019-02-12 22:55:35 -0500

    Some changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -14,9 +14,11 @@ export class FooterComponent {
     constructor (private contentfulService: ContentfulService)
       {}
       validateAgain:boolean = true;
+      showMap:boolean = false;
       blogPosts: Entry<any>[];
       footerContent: Entry<any>;
       ourPromises: Entry<any>;
+      siteMap: Entry<any>;
      
       ngOnInit() {
         this.contentfulService.getBlogPosts()
@@ -28,7 +30,8 @@ export class FooterComponent {
           this.contentfulService.getObjectLists()
           .then(
             lists => {
-            this.ourPromises = lists.filter(list=>list.fields.type === 'aboutFooter').pop()
+            this.ourPromises = lists.filter(list=>list.fields.type === 'aboutFooter').pop(),
+            this.siteMap = lists.filter(list=>list.fields.type === 'sitemap').pop()
             }
         )
       }
@@ -37,5 +40,16 @@ export class FooterComponent {
         if (this.blogPosts && this.validateAgain) {
           this.validateAgain = !this.validateAgain
         }  
+      }
+
+      siteMapClick() {
+        let scrollToTop = window.setInterval(() => {
+          let pos = window.pageYOffset;
+          if (pos > 0) {
+              window.scrollTo(0, pos - 1000); // how far to scroll on each step
+          } else {
+              window.clearInterval(scrollToTop);
+          }
+      }, 5);
       }
 }
